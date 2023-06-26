ae28ef9cb7f04b779622af458d7eba9825ad2eae
Athalia Rupilu <101616548+athaleeah@users.noreply.github.com>
2023-06-26 17:43:14 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Contact from './component/Contact';
+import Education from './component/Education';
+import Routing from './Routing';
+
+function App() {
+  // atha
+  return (
+    <div className="App-header">
+  <Routing/>
+  
+   
+    </div>
+  );
+}
+
+export default App;
